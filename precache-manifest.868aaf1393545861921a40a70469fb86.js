82831b569262c8b16ca5ed1c55eb59b04a400cc1
mgrinko <glukym@gmail.com>
2019-02-20 17:59:25 +0200

    Updates

--- precache-manifest.868aaf1393545861921a40a70469fb86.js
+++ precache-manifest.868aaf1393545861921a40a70469fb86.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "bf5155cd4a3060f2596b",
+    "revision": "91d0106b71673b3b0d25",
     "url": "/cra-demo/static/css/main.50a8489b.chunk.css"
   },
   {
-    "revision": "bf5155cd4a3060f2596b",
-    "url": "/cra-demo/static/js/main.bf5155cd.chunk.js"
+    "revision": "91d0106b71673b3b0d25",
+    "url": "/cra-demo/static/js/main.91d0106b.chunk.js"
   },
   {
     "revision": "e87b5116d9f5545e0c66",
     "url": "/cra-demo/static/js/runtime~main.e87b5116.js"
   },
   {
-    "revision": "063ce5bc9f8a30d66648",
-    "url": "/cra-demo/static/js/2.063ce5bc.chunk.js"
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/cra-demo/static/js/2.ba1ca5ba.chunk.js"
   },
   {
     "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
     "url": "/cra-demo/static/media/logo.5d5d9eef.svg"
   },
   {
-    "revision": "c999910392fc3576698bc7e6fc5fdc41",
+    "revision": "ef93c0f5da6d4fed6b21c79db03a2dc7",
     "url": "/cra-demo/index.html"
   }
 ];
